d1b2b46a8cebf71160545a9677b6f8df75ee0b52
cocojambo <jakubstanislawnowak@gmail.com>
2018-10-17 15:18:09 +0200

    dodane testowe klasy lokalizacji.

--- gra.py
+++ gra.py
@@ -11,7 +11,8 @@ battle_dict = {
     'CA':0, 'CB':-1, 'CC':0, 'CD':1,
     'DA':1, 'DB':0, 'DC':-1, 'DD':0
     }
-
+PLAYERONE = 'KAPITANBOMBA' #DLA TESTU
+CURRENT_LOCATION = 'OPUSZCZONACHATA' #DLA TESTU
 
 ################################################################################
 #KLASY BOHATERÓW
@@ -108,9 +109,9 @@ class attack(object):
         #siła obrony potwora + randint
         self.monster_defence_power = ENEMY.defence + random.randint(0, 3)
         if self.hero_attack_power >= self.monster_defence_power:
-            print("win")
+            print("win")###trzeba dopisać
         elif self.attack_power < self.defence_power:
-            print("loose")
+            print("loose")###trzeba dopisać
 
 #używane, kiedy potwór zaatakuje bohatera
 class defence(attack):
@@ -123,15 +124,70 @@ class defence(attack):
         #siła obrony bohatera czyli premia za pancerz i lvl + randint
         self.hero_defence_power = PLAYER.armor_power + random.rabdint(0, 3)
         if self.monster_attack_power >= self.hero_defence_power:
-            print("loose")
+            print("loose")#treba dopisać
         elif self.monster_attack_power < self.hero_defence_power:
-            print("win")
+            print("win")#trzeba dopisać
 
-# (_)_)==================D TEST
-#def test():
-#    a = KAPITANBOMBA(20, 10, 'karabinlaserowy', 'hełm')
-#    a.attack_power()
-#    a.defence_power()
-#    return a
 
-#a = test()
+class run(object):
+    def REACTION(self):
+        chance = random.randint(0, 9)
+        if chance <= 2:
+            defence(PLAYERONE, CURRENT_LOCATION.spawn )
+        else:
+            pass
+            #next location
+
+##CURRENT LOCATION
+
+
+################################################################################
+#KLASY LOKACJI
+################################################################################
+class PLACE(object):
+    FIRST_ACTION = ['MONSTER', 'ITEM', 'NOTHING', 'QUEST', 'BOX']
+    monsters = ['KURWINOX', 'SKURWOL', 'KUTANOID', 'CQRWOZAUR']
+    def MONSTER(self):
+        print('pojawił się')#losowa historyjka
+        spawn = getattr(self, random.choice(self.FIRST_ACTION))
+        print(spawn)
+        #drukuj parametry potwora
+        print("""
+        Co robisz?
+        1. Omiń potwora
+        2. Zaatakuj potwora
+        """)
+        ACTION = input("> ")
+        if ACTION == '1' or ACTION == '1.':
+            a = attack()
+            a(PLAYERONE, self.spawn)
+        elif ACTION == '2' or ACTION =='2.':
+            run.REACTION()
+    def ITEM(self):
+        print('test2')
+    def NOTHING(self):
+        print('test3')
+    def QUEST(self):
+        print('test4')
+    def BOX(self):
+        print('test5')
+
+class OPUSZCZONACHATA(PLACE):
+    def __init__(self):
+        print("""
+        początkowa historyjka blablabla
+        czy chcesz wejść do pomieszczenia?
+        TAK/NIE
+        """)
+
+        while True:
+            DECYZJA = input("> ")
+            if DECYZJA == 'TAK' or DECYZJA == 'tak' or DECYZJA == 'T' or DECYZJA == 't':
+                    chc= getattr(self, random.choice(self.FIRST_ACTION))
+                    chc()
+
+            elif DECYZJA == 'NIE' or DECYZJA == 'nie' or DECYZJA == 'N' or DECYZJA == 'n':
+                exit()
+
+            else:
+                print("Błędny wybór. Wybierz TAK/NIE")
